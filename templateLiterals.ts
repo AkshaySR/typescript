507c1dc44778b7a52efb9b08cdfc188c1f8e0a9f
Akshay <asr.asrcode@gmail.com>
2018-11-15 22:22:25 -0500

    template literals

---
+++ templateLiterals.ts
@@ -0,0 +1,8 @@
+const carName = "Subaru";
+const carIHave = "I have a " + carName + "car of 2018 model";
+
+
+//with template literals we can use multi line string easily
+const  templateLiteral = `I have a  ${carName} 
+    car of 2018 model`;
+console.log(templateLiteral);
